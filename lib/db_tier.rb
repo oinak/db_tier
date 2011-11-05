496892a3941474e682ee0278103fa65c5b383ba8
Fernando Martínez <fernando.mdelacueva@gmail.com>
2011-11-05 12:55:52 +0100

    Preliminar version that works but fails to inject the before_filter

--- lib/db_tier.rb
+++ lib/db_tier.rb
@@ -1,2 +1,29 @@
 module DbTier
+  # intercept ActiveRecord#establish_connection intended config to make it use our params
+
+  # instantiate in you initializer to define config retrieving method
+  class Config
+    def self.init(config_hash=nil, &block)
+      @config_hash = config_hash
+      @config_method = block if block_given?
+
+      ApplicationController.instance_eval do
+
+        protected
+
+        def db_tier_select
+          raise 'ey'
+          # Establish the request specific connection
+          ActiveRecord::Base.establish_connection( DbTier::Config.retrieve )
+        end
+
+        public
+        before_filter :db_tier_select
+      end
+    end
+
+    def self.retrieve
+      @config_hash || @config_method.call
+    end
+  end
 end
