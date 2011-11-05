6a1618af3f68fd5510c66e117112b6f7f21ea30e
Fernando Martínez <fernando.mdelacueva@gmail.com>
2011-11-05 12:58:51 +0100

    Dummy test application with one Post resource

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  # The following code must be injected from the gem:
+  def db_tier_select
+    # Establish the request specific connection
+    ActiveRecord::Base.establish_connection( DbTier::Config.retrieve )
+  end
+
+  public
+  before_filter :db_tier_select
 end
+
---
+++ test/dummy/config/initializers/db_tier.rb
@@ -0,0 +1,3 @@
+test_config = {:adapter=>"sqlite3", :database=>"/home/fernando/src/db_tier/test/dummy/db/development.sqlite3", :pool=>5, :timeout=>5000}
+
+DbTier::Config.init(test_config)
